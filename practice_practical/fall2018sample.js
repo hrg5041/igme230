d3cfe130cc8d49f88915ea2556e754ce9aaf7042
Henry Grant <hrg5041@rit.edu>
2018-12-05 16:28:50 -0500

    practical practice

--- practice_practical/fall2018sample.js
+++ practice_practical/fall2018sample.js
@@ -1,1 +1,16 @@
 /* Don't use <script> tags in a linked js file! */
+$( "#one").click(function() {
+    $( "#one.submenu" ).slideToggle( "slow");
+  });
+  $( "#two").click(function() {
+    $( "#two.submenu" ).slideToggle( "slow");
+  });
+
+  let header = "content1.txt";
+  $("#choose-content").val(header);
+  $("#content").load(header);
+
+  $("#choose-content").change(function() {
+    header = $(this).val(); // changes dependent on the text file being chosen 
+    $("#content").load(header); //loads the specific verse that is selected
+});
